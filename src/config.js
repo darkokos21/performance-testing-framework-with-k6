5860b2dc0202756070ce894c0ec1fe7aa688baa1
darkokos21 <darkokostov@hotmail.com>
2025-12-12 10:43:26 +0100

    Update config.js thresholds

--- src/config.js
+++ src/config.js
@@ -20,7 +20,7 @@ export const config = {
     ],
   },
   thresholds: {
-    http_req_duration: ["p(95)<500"],
-    http_req_failed: ["rate<0.02"]
+    http_req_duration: ["p(95)<800"],
+    http_req_failed: ["rate<0.05"]
   },
 };
